e81145ae2d6e41c111d9daf7458cbbafbfa8d8e3
Patrice Jada <patricemusoke@gmail.com>
2024-07-29 17:02:01 -0400

    Creating File Structure and Sample Target for qtpy

---
+++ projects/qtpy/model_fuzzer.py
@@ -0,0 +1,29 @@
+import atheris
+import sys
+from qtpy.QtWidgets import QApplication, QTextEdit
+from qtpy.QtCore import Qt
+
+# Define a minimal widget class for testing
+class TestConsoleWidget(QTextEdit):
+    def __init__(self):
+        super().__init__()
+
+    def process_input(self, data):
+        # For simplicity, we'll use QTextEdit's method for setting text
+        self.setPlainText(data.decode('utf-8', 'ignore'))
+
+def fuzz(data):
+    app = QApplication(sys.argv)
+
+    # Create an instance of the TestConsoleWidget
+    widget = TestConsoleWidget()
+    
+    # Process the input data
+    widget.process_input(data)
+
+def main():
+    atheris.Setup(sys.argv, fuzz)
+    atheris.Fuzz()
+
+if __name__ == "__main__":
+    main()
